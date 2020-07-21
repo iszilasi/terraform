cd262539768020863cb97b5a7b02ff91cd1d011b
v-iszilasi <v-iszilasi@hotels.com>
2020-07-21 13:24:06 -0500

    using modules

--- aws_jump/main.tf
+++ aws_jump/main.tf
@@ -3,9 +3,38 @@ provider "aws" {
   region = "us-east-1"
 }
 
+
+resource "aws_security_group" "allow_22" {
+  name        = "allow_22"
+  description = "Allow 22 from world"
+  vpc_id      = "vpc-8237d5ff"
+
+  ingress {
+    description = "incoming 22"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_22"
+  }
+}
+
+
 resource "aws_instance" "jump" {
   ami = "ami-08f3d892de259504d" 
   instance_type = "t2.micro"
   key_name="ec2_default"
-  security_groups=["launch-wizard-4"]
+  security_groups=["allow_22"]
 }
+
+
